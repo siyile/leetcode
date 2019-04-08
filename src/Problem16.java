44dba150995d234a5c22d393b0d494622f07304c
siyi.le <seayahh@outlook.com>
2019-04-08 18:41:24 +0800

    working on p16

---
+++ src/Problem16.java
@@ -0,0 +1,10 @@
+public class Problem16 {
+    public int threeSumClosest(int[] nums, int target) {
+        return target;
+    }
+    public static void main(String [] args){
+        Problem16 p = new Problem16();
+        int [] nums = {2, 3};
+        System.out.println(p.threeSumClosest( nums, 3));
+    }
+}
