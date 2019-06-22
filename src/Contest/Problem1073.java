79b84dc22e8a87ceb6b02833750034f1d0c0a36a
siyi.le <seayahh@outlook.com>
2019-06-22 14:33:10 +0800

    finish 6/21 task

---
+++ src/Contest/Problem1073.java
@@ -0,0 +1,40 @@
+package Contest;
+
+import java.util.*;
+
+public class Problem1073 {
+    public int[] addNegabinary(int[] arr1, int[] arr2) {
+        List<Integer> ans = new ArrayList<>();
+        int i = arr1.length - 1;
+        int k = arr2.length - 1;
+        while (i >= 0 || k >= 0) {
+            ans.add((i >= 0 ? arr1[i--] : 0) + (k >= 0 ? arr2[k--] : 0));
+        }
+        ans.add(0);
+        ans.add(0);
+        for (int j = 0; j < ans.size(); j++) {
+            if (ans.get(j) > 1) {
+                if (ans.get(j + 1) > 0) {
+                    ans.set(j + 1, ans.get(j + 1) - 1);
+                } else {
+                    ans.set(j + 1, ans.get(j + 1) + 1);
+                    ans.set(j + 2, ans.get(j + 2) + 1);
+                }
+                ans.set(j, ans.get(j) - 2);
+            }
+        }
+        for (int j = ans.size() - 1; j > 0 && ans.get(j) == 0; j--) {
+            ans.remove(j);
+        }
+        int[] res = new int[ans.size()];
+        for (int j = 0; j < ans.size(); j++) {
+            res[j] = ans.get(ans.size() - 1 - j);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Problem1073 p = new Problem1073();
+        Arrays.stream(p.addNegabinary(new int[]{1}, new int[]{1})).forEach(System.out::println);
+    }
+}
---
+++ src/Problem232.java
@@ -0,0 +1,53 @@
+import java.util.Stack;
+
+public class Problem232 {
+    class MyQueue {
+
+        /** Initialize your data structure here. */
+        public MyQueue() {
+            s1 = new Stack<>();
+            s2 = new Stack<>();
+        }
+
+        /** Push element x to the back of queue. */
+        public void push(int x) {
+            if (s1.isEmpty()) front = x;
+            s1.push(x);
+        }
+
+        /** Removes the element from in front of queue and returns that element. */
+        public int pop() {
+            if (s2.isEmpty()) {
+                while (!s1.isEmpty()) {
+                    s2.push(s1.pop());
+                }
+            }
+            return s2.pop();
+        }
+
+        /** Get the front element. */
+        public int peek() {
+            if (!s2.isEmpty()) {
+                return s2.peek();
+            }
+            return front;
+        }
+
+        /** Returns whether the queue is empty. */
+        public boolean empty() {
+            return s1.isEmpty() && s2.isEmpty();
+        }
+
+        Stack<Integer> s1, s2;
+        int front;
+    }
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
+}
