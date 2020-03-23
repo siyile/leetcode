12ffe56ea4fc3214d9673aeeceaeb4e2f1c08422
siyi.le <seayahh@outlook.com>
2020-03-22 21:56:30 -0700

    review contest & 306 done

--- src/UpTo50/Problem28.java
+++ src/UpTo50/Problem28.java
@@ -1,26 +1,52 @@
 package UpTo50;
 
+import java.util.Arrays;
+
 public class Problem28 {
     public int strStr(String haystack, String needle) {
-        if (needle.equals("")) {
-            return 0;
-        }
-        int alen = haystack.length();
-        int blen = needle.length();
-        if (alen < blen) return -1;
-        char [] a = haystack.toCharArray();
-        char [] b = needle.toCharArray();
+        int[] lps = getLPS(needle);
+        System.out.println(Arrays.toString(lps));
 
-        for (int i = 0; i < alen - blen + 1; i++) {
-            int j = 0;
-            int p = i;
-            while (j < blen && a[p++] == b[j++]) {
-                if (j == blen) {
-                    return p - j;
-                }
+        int N = haystack.length();
+        int M = needle.length();
+
+        int i = 0, j = 0;
+
+        while (i < N) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+            }
+            if (j == M) {
+                return i - j;
+            } else if (i < N && haystack.charAt(i) != needle.charAt(j)) {
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i += 1;
             }
         }
         return -1;
+    }
+
+    private int[] getLPS(String pattern) {
+        int len = 0;
+        int[] lps = new int[pattern.length()];
+        int i = 1;
+        while (i < pattern.length()) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len == 0) {
+                    i++;
+                } else {
+                    len = lps[len - 1];
+                }
+            }
+        }
+        return lps;
     }
 
     public static void main(String [] args) {
