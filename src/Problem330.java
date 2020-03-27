01abc87248f262036a72163a012d541d86ec7183
siyi.le <seayahh@outlook.com>
2020-03-26 20:40:25 -0700

    330 331 333 done

---
+++ src/Problem330.java
@@ -0,0 +1,15 @@
+public class Problem330 {
+    public int minPatches(int[] nums, int n) {
+        long miss = 1;
+        int i = 0, added = 0;
+        while (miss <= n) {
+            if (i < nums.length && nums[i] <= miss) {
+                miss += nums[i++];
+            } else {
+                miss += miss;
+                added += 1;
+            }
+        }
+        return added;
+    }
+}
