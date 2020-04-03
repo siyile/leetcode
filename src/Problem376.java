94b77721bb3555e03649977f8dcca39d68610a37
siyi.le <seayahh@outlook.com>
2020-04-02 17:03:00 -0700

    376 377 done

---
+++ src/Problem376.java
@@ -0,0 +1,40 @@
+public class Problem376 {
+    // greedy
+    public int wiggleMaxLength1(int[] nums) {
+        if (nums.length < 2) return nums.length;
+        int len1 = 1, len2 = 1;
+        boolean big1 = true, big2 = false;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                if (big1) {
+                    len1++;
+                    big1 = false;
+                }
+                if (big2) {
+                    len2++;
+                    big2 = false;
+                }
+            } else if (nums[i] < nums[i - 1]) {
+                if (!big1) {
+                    len1++;
+                    big1 = true;
+                }
+                if (!big2) {
+                    len2++;
+                    big2 = true;
+                }
+            }
+        }
+        return Math.max(len1, len2);
+    }
+
+    public int wiggleMaxLength(int[] nums) {
+        if (nums.length == 0) return 0;
+        int up = 1, down = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]) down = up + 1;
+            else if (nums[i] > nums[i - 1]) up = down + 1;
+        }
+        return Math.max(up, down);
+    }
+}
---
+++ src/Problem379.java
@@ -0,0 +1,26 @@
+public class Problem379 {
+    private class PhoneDirectory {
+
+        /** Initialize your data structure here
+         @param maxNumbers - The maximum numbers that can be stored in the phone directory. */
+        public PhoneDirectory(int maxNumbers) {
+
+        }
+
+        /** Provide a number which is not assigned to anyone.
+         @return - Return an available number. Return -1 if none is available. */
+        public int get() {
+
+        }
+
+        /** Check if a number is available or not. */
+        public boolean check(int number) {
+
+        }
+
+        /** Recycle or release a number. */
+        public void release(int number) {
+
+        }
+    }
+}
