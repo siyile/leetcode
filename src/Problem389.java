ab8023ec2d61e1a5c86b8926e0b1ea81426867df
siyi.le <seayahh@outlook.com>
2020-04-04 16:42:14 -0700

    396 done

--- src/Problem389.java
+++ src/Problem389.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Problem389 {
+    public char findTheDifference(String s, String t) {
+        int sum = 0;
+        for (char ch :
+                t.toCharArray()) {
+            sum += ch;
+        }
+
+        for (char ch :
+                s.toCharArray()) {
+            sum -= ch;
+        }
+
+        return (char) sum;
+    }
 }
--- src/Problem392.java
+++ src/Problem392.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Problem392 {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+        while (i < s.length()) {
+            while (j < t.length() && s.charAt(i) != t.charAt(j)) {
+                j++;
+            }
+            if (j == t.length()) {
+                return false;
+            }
+            j++;
+            i++;
+        }
+
+        return i == s.length();
+    }
 }
